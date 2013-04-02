78de23638537cfd79a198f1ef7b749777366bec0
Alexander Pokluda <apokluda@gmail.com>
2013-04-02 08:47:43 -0400

    Skeleton for dnsgw_test_client

---
+++ dnsgw_test_client/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2013-04-02
+ *      Author: alex
+ */
+
+
+int main( int argc, char const* argv[] )
+{
+return 0;
+}
+
---
+++ dnsgw_test_client/stdhdr.hpp
@@ -0,0 +1,14 @@
+/*
+ * stdhdr.hpp
+ *
+ *  Created on: 2013-04-02
+ *      Author: alex
+ */
+
+#ifndef STDHDR_HPP_
+#define STDHDR_HPP_
+
+
+
+
+#endif /* STDHDR_HPP_ */
